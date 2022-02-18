fa65395108e76b9efe097911efda1fb2951c1207
Atul kumar Agrawal <ms.atul1303@gmail.com>
2022-02-18 16:29:07 +0530

    Update multithreading4.py

--- Python/multithreading4.py
+++ Python/multithreading4.py
@@ -22,6 +22,9 @@ def do_something(seconds):
 with concurrent.futures.ThreadPoolExecutor() as executor:
     secs = [5, 4, 3, 2, 1]
     results = executor.map(do_something, secs)
+    
+    for result in results:
+        print(result)
 
 finish = time.perf_counter()
 
