ca847d0bcb7b59d27a11098ae7cbbd5ed36b45d7
Atul kumar Agrawal <ms.atul1303@gmail.com>
2022-02-18 16:15:27 +0530

    Update multithreading2.py

--- Python/multithreading2.py
+++ Python/multithreading2.py
@@ -1,1 +1,31 @@
+#Using:
+#1. concurrent.futures library
+#Using ThreadPoolExecutor() as executor
+#Using :
+#submit method: If we want to execute a function once at a time - Submit method executes a function once at a time and retuns a future object
+#Finally, print results of each submit using as_completed() method
 
+import concurrent.futures
+import time
+
+start = time.perf_counter()
+
+
+def do_something(seconds):
+    print(f'Sleeping {seconds} second(s)...')
+    time.sleep(seconds)
+    return f'Done Sleeping...{seconds}'
+
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = [executor.submit(do_something, 1) for _ in range(10)]
+    for f in concurrent.futures.as_completed(results):
+      print(f.result)
+
+    # for result in results:
+    #     print(result)
+ 
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} second(s)')
