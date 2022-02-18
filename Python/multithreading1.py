ab23e20c21fc941284869eee9754019c8139906b
Atul kumar Agrawal <ms.atul1303@gmail.com>
2022-02-18 15:45:04 +0530

    Update multithreading1.py

--- Python/multithreading1.py
+++ Python/multithreading1.py
@@ -1,1 +1,28 @@
+#basic multithreading using 
+#1. "threading" library
+#2. thread.start() and thread.join()
 
+import threading
+import time
+
+start = time.perf_counter()
+
+
+def do_something(seconds):
+    print(f'Sleeping {seconds} second(s)...')
+    time.sleep(seconds)
+    return f'Done Sleeping...{seconds}'
+
+threads_list = []
+
+for _ in range(10):
+    t = threading.Thread(target=do_something, args=[1.5])
+    t.start()
+    threads_list.append(t)  
+  
+for thread in threads_list:
+  thread.join()
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} second(s)')
