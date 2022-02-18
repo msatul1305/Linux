f9c396254987bcdcdfbb1da63ca88a852e9250a3
Atul kumar Agrawal <ms.atul1303@gmail.com>
2022-02-18 18:17:29 +0530

    Update asyncio1.py

--- Python/asyncio1.py
+++ Python/asyncio1.py
@@ -1,1 +1,20 @@
+import asyncio
 
+async def fetch_data():
+  print("start fetching")
+  await asyncio.sleep(2)
+  print("done fetching")
+  return {'data':1}
+
+async def print_numbers():
+  for i in range(10):
+    print(i)
+    await asyncio.sleep(0.5)
+
+async def main():
+  task2 = asyncio.create_task(print_numbers())
+  await fetch_data()
+  
+  await task2
+
+asyncio.run(main())
