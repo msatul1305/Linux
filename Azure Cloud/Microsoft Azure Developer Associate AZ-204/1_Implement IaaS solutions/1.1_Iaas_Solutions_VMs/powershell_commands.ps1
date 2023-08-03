74c2657d390139c21bc1ff7df1fbac77e502a3a9
msatul1305 <ms.atul1303@gmail.com>
2023-08-03 19:39:18 +0530

    Updated Azure

--- Azure Cloud/Microsoft Azure Developer Associate AZ-204/1_Implement IaaS solutions/1.1_Iaas_Solutions_VMs/powershell_commands.ps1
+++ Azure Cloud/Microsoft Azure Developer Associate AZ-204/1_Implement IaaS solutions/1.1_Iaas_Solutions_VMs/powershell_commands.ps1
@@ -1,17 +1,12 @@
 # All powershell commands are of the syntax: "Verb-Action"
 # Check if az powershell is installed:
 get-command az
+
 # Install Azure PowerShell on Windows: install-module -name az -allowclobber  (allowclobber means forced update rewriting prev installation)
 Get-ExecutionPolicy -List
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Install-Module -Name Az -Repository PSGallery -Force
 Update-Module -Name Az -Force
-# 2. Sign in:
-#    -
-
-
-# Deploy ARM template via Powershell:
-New-AzResourceGroupDeployment -Name mydeployment -ResourceGroupName 'rgroup' -TemplateFile './template/template.json' -TemplateParameterFile './template/parameter.json'
 
 # Using Azure Powershell to deploy VM:
 # Steps:
@@ -33,3 +28,7 @@ New-AzVM -ResourceGroupName 'rgroup2' -Name 'vmname' -Image 'Ubuntu2204' -size "
 Get-AzPublicIpAddress -ResourceGroupName 'rgroup' -Name 'vmname' | Select-Object IpAddress
 # To delete resource group:
 Remove-AzResourceGroup -Name 'rgroup'
+
+# Deploy ARM template via Powershell:
+New-AzResourceGroupDeployment -Name mydeployment -ResourceGroupName 'rgroup' -TemplateFile './template/template.json' -TemplateParameterFile './template/parameter.json'
+
--- Azure Cloud/Microsoft Azure Developer Associate AZ-204/common_azure_cli_commands.sh
+++ Azure Cloud/Microsoft Azure Developer Associate AZ-204/common_azure_cli_commands.sh
@@ -1,6 +1,11 @@
+# List of all resource groups available
+az group list --output table
+
+# Delete resource group:(All resources in the resource group will be deleted as well)
+az group delete --name "rgroup-name"
+
 # To delete all resource groups
 for rgname in `az group list -o tsv`; do
 echo Deleting ${rgname}
 az group delete -n ${rgname} --yes --no-wait
 done
-
