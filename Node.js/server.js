b8ce9c0aa1a154dbd0cbad784d17ac11fc2079a3
msatul1305 <ms.atul1303@gmail.com>
2023-11-11 00:34:32 +0530

    updated server.js

--- Node.js/server.js
+++ Node.js/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
