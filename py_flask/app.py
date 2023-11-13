df2eb2317f4e82a0d88026c4fe36afaff14d53ce
msatul1305 <ms.atul1303@gmail.com>
2023-11-13 19:00:09 +0530

    updated requirements

--- py_flask/app.py
+++ py_flask/app.py
@@ -8,23 +8,30 @@ logging.basicConfig(level=logging.DEBUG,  # Set the logging level (DEBUG, INFO, 
 app = Flask(__name__)
 
 
-def longest_palindromic_arrangement(files):
-    answer = 'NULL'
-    return {"answer": answer}
+def longest_palindromic_arrangement(s):
+    countr = {}
+    for char in s:
+        countr[char] = countr.get(char, 0) + 1
+    len = 0
+    is_odd = False
+    for c in countr.values():
+        len += c // 2 * 2
+        if c % 2 == 1:
+            is_odd = True
+    if is_odd:
+        len += 1
+    return len
 
 
 @app.route('/file-reorganization', methods=['POST'])
 def file_reorganization():
-    try:
-        data = request.get_json()
-        logging.info(data)
-        if "inputs" in data and isinstance(data["inputs"], list):
-            result = longest_palindromic_arrangement(data["inputs"])
-            return jsonify(result)
-        else:
-            return jsonify({"error": "Invalid input format"}), 400
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    data = request.json
+    inputs = data.get('inputs', [])
+    results = []
+    for i in inputs:
+        result = longest_palindromic_arrangement(i)
+        results.append(result)
+    return jsonify({"answer": results})
 
 
 if __name__ == '__main__':
