44d29dc7e7a35bbf1581bfea46979b9f53c4377f
msatul1305 <ms.atul1303@gmail.com>
2023-11-14 23:38:24 +0530

    updated

--- py_flask/app.py
+++ py_flask/app.py
@@ -2,45 +2,29 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-def get_time_intervals(inputs):
-    result = []
+def max_risk_mitigated(inputs):
+    results = []
+    for input_data in inputs:
+        n, m = map(int, input_data[0].split())
+        costs = list(map(int, input_data[1].split()))
 
-    for input_set in inputs:
-        n, *employee_names_and_shifts = input_set
-        n = int(n)
+        max_risk = 0
+        for i in range(m):
+            for j in range(i + 1, m):
+                risk_mitigated = costs[j] - costs[i]
+                if risk_mitigated > 0:
+                    max_risk = max(max_risk, risk_mitigated)
 
-        shifts = [(int(employee_names_and_shifts[i]), int(employee_names_and_shifts[i + 1]), employee_names_and_shifts[i + 2]) for i in range(0, len(employee_names_and_shifts), 3)]
+        results.append(max_risk)
 
-        time_intervals = set()
-        for _, end_time, _ in shifts:
-            time_intervals.add(_)
-            time_intervals.add(end_time)
+    return {"answer": results}
 
-        time_intervals = sorted(list(time_intervals))
-
-        output_intervals = []
-        for i in range(len(time_intervals) - 1):
-            start_time, end_time = time_intervals[i], time_intervals[i + 1]
-            employees_at_desk = []
-
-            for employee_name, shift_start, shift_end in shifts:
-                if shift_start <= start_time < shift_end or shift_start < end_time <= shift_end:
-                    employees_at_desk.append(employee_name)
-
-            employees_at_desk.sort()
-            output_intervals.append((start_time, end_time, len(employees_at_desk), " ".join(employees_at_desk)))
-
-        result.append([len(output_intervals)] + output_intervals)
-
-    return {"answer": result}
-
-@app.route('/time-intervals', methods=['POST'])
-def time_intervals():
+@app.route('/risk-mitigation', methods=['POST'])
+def risk_mitigation():
     try:
-        data = request.json
-        inputs = data["inputs"]
-        response = get_time_intervals(inputs)
-        return jsonify(response)
+        data = request.json['inputs']
+        result = max_risk_mitigated(data)
+        return jsonify(result)
     except Exception as e:
         return jsonify({"error": str(e)})
 
