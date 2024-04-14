75585f4eca8ca9fe947b84ab7c8f197647888201
Atul <ms.atul1303@gmail.com>
2024-04-14 11:48:38 +0530

    update readme

--- Python/fluent_python/1card_deck.py
+++ Python/fluent_python/1card_deck.py
@@ -6,6 +6,7 @@ print(Card)
 
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    print(ranks)
     suits = 'spades diamonds clubs hearts'.split()
 
     def __init__(self):
@@ -32,4 +33,23 @@ print(deck[::13])  # print all 2
 for card in deck:
     print(card)
 for card in reversed(deck):
+    print(card)
+print(Card('Q', 'hearts') in deck)
+print(Card('A', 'hearts') in deck)
+print(Card('2', 'hearts') in deck)
+
+# sort cards by power -> A>K>Q>J>10..., Spades>Hearts>Diamond>Clubs
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+print(suit_values)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    print(card, card.rank, rank_value, card.suit)
+    print(rank_value * len(suit_values) + suit_values[card.suit])
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+for card in sorted(deck, key=spades_high):
     print(card)
