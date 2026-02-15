369fdaa66d5aeccd9144f1fe96430ec37bba46c6
msatul1305 <ms.atul1303@gmail.com>
2026-02-15 15:31:09 +0530

    update paths

--- smart-portfolio/vite.config.ts
+++ smart-portfolio/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/smart-portfolio/',
+  base: '/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
